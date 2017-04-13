5e5cab1b499e6122aaf0c610106fd1a6d15fd59d
petercat12 <Pwensel@gmail.com>
2017-04-13 00:58:32 -0500

    change url to something more appropriate.

--- OmegaPricing/api/urls.py
+++ OmegaPricing/api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import sales_api
 
 urlpatterns = [
-    url(r'^user/login/?$', sales_api.GetUpdatedSalesRecords.as_view(), name='updateSales'),
+    url(r'^products/update/?$', sales_api.GetUpdatedSalesRecords.as_view(), name='updateSales'),
 ]
